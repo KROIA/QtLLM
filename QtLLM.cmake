b9d25b537aca7625345bf224c81de3feb14b06b1
Alex Krieg <alexkrieg@gmx.ch>
2026-05-02 00:23:48 +0200

    + cmake dependency file for usage

---
+++ QtLLM.cmake
@@ -0,0 +1,20 @@
+## description: Library to add a Large Language Model Interface. Specific tools can be implemented that can be called by the LLM
+
+function(dep LIBRARY_MACRO_NAME SHARED_LIB STATIC_LIB STATIC_PROFILE_LIB INCLUDE_PATHS)
+    # Define the git repository and tag to download from
+    set(LIB_NAME QtLLM)
+    set(LIB_MACRO_NAME QTLLM_LIBRARY_AVAILABLE)
+    set(GIT_REPO https://github.com/KROIA/QtLLM.git)
+    set(GIT_TAG main)
+	set(NO_EXAMPLES True)	
+	set(NO_UNITTESTS True)
+	set(ADDITIONAL_INCLUDE_PATHS )
+
+	downloadStandardLibrary()
+endfunction()
+
+dep(DEPENDENCY_NAME_MACRO 
+    DEPENDENCIES_FOR_SHARED_LIB 
+    DEPENDENCIES_FOR_STATIC_LIB 
+    DEPENDENCIES_FOR_STATIC_PROFILE_LIB 
+    DEPENDENCIES_INCLUDE_PATHS)
